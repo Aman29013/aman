7acd69dd7999d6528b0708084166cc5cc9b84b5d
Aman29013 <amankushwaha2901prayagraj@gmail.com>
2023-09-16 00:17:32 +0530

    first

---
+++ aman.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+   
+    printf(" numbers\n");
+    scanf("%d%d",&a,&b);
+     c=apow(b);
+    printf("multiply is:=%d/%d=%d",a,b,c);
+
+
+
+return 0;}
